43d0f3c1740e06a0c79a169a6c09e8b64b3e7ac1
arshia <arshia.karimi@gmail.com>
2023-10-29 17:23:57 +0330

    App-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const name = <h1>web</h1>;
   return (
-    <div>
-      {name}
-      {name}
-      {name}
-      {name}
+    <div className="App">
+      arshia
     </div>
   );
 }
