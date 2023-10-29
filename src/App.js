18e2c890c8fe2c286ae10776efb69480b7363a62
arshia <arshia.karimi@gmail.com>
2023-10-29 17:25:25 +0330

    App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      arshia
+    <div>
+      <User name="Thor" age={22} email="arshia.karimi@email.com" />
+      <Job salary={1000000000} position="developer" company="google" />
     </div>
   );
 }
+
+// const GetNaame = (name) => {
+//   return name;
+// };
+
+// const props = {
+//   name: "Thor",
+//   age: 7,
+//   email: "arshia.karimi@gmali.com"
+// };
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h3>{props.email}</h3>
+    </div>
+  );
+};
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h3>{props.company}</h3>
+    </div>
+  );
+};
 
 export default App;
