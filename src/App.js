eae23104bdb532b0cc06ebaf1ce4e8350d31bb24
arshia <arshia.karimi@gmail.com>
2023-10-30 15:56:57 +0330

    age

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import "./App.css";
 
 function App() {
+  const age = 20;
+
   return (
-    <div>
-      <User name="Thor" age={22} email="arshia.karimi@email.com" />
-      <Job salary={1000000000} position="developer" company="google" />
+    <div className="App">
+      {age > 20 ? <h1>over</h1> : <h1>under</h1>}
+
+      {age === 20 && <h1>age is 20</h1>}
+
+      <button type="submit" style={{ fontSize: age > 20 ? "20px" : "30px" }}>
+        click me
+      </button>
     </div>
   );
 }
-
-// const GetNaame = (name) => {
-//   return name;
-// };
-
-// const props = {
-//   name: "Thor",
-//   age: 7,
-//   email: "arshia.karimi@gmali.com"
-// };
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  );
-};
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.company}</h3>
-    </div>
-  );
-};
 
 export default App;
