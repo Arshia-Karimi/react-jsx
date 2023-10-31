e910ace6be2e4d7dece410867e5c71a64bc2e9a7
arshia <arshia.karimi@gmail.com>
2023-10-31 17:22:56 +0330

    a-b

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import { User } from "./User.js";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const users = [
-    { name: "a", age: 23 },
-    { name: "b", age: 33 },
-    { name: "c", age: 31 },
+  const [age, setAge] = useState(0);
+  const increaseAge = () => {
+    if (age < 5) {
+      setAge(age + 1);
+    }
+  };
+  const decreaseAge = () => {
+    if (age > 0) {
+      setAge(age - 1);
+    }
+  };
+  const names = [
+    { name: "html", finished: true },
+    { name: "css", finished: false },
+    { name: "javascript", finished: true },
+    { name: "react", finished: false },
+    { name: "python", finished: true },
   ];
 
   return (
     <div className="App">
-      {users.map((user, index) => {
-        return <User key={index} name={user.name} age={user.age} />;
+      {names.map((a, b) => {
+        return a.finished && <h2 key={b}>{a.name}</h2>;
       })}
+      <h1>{age}</h1>
+      <button className="btn" onClick={increaseAge}>
+        Increase age +
+      </button>
+      <button className="btn" onClick={decreaseAge}>
+        Increase age -
+      </button>
     </div>
   );
 }
