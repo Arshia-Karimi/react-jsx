34192043573d9ad8d2921aa3e59a14fb0d2a50df
arshia <arshia.karimi@gmail.com>
2023-11-28 17:19:00 +0330

    fact

--- src/Fact.js
+++ src/Fact.js
@@ -1,14 +1,28 @@
-import { Axios } from "axios";
-import { useState } from "react";
+import Axios from "axios";
+import { useReducer, useState } from "react";
+import { factReducer, initialState } from "./factReducer";
 
 export const Fact = () => {
-  const [loading, setLoading] = useState(false);
-  const [fact, setFact] = useState("");
-  const [error, setError] = useState(false);
+  const [state, dispatch] = useReducer(factReducer, initialState);
 
   const handelFetch = () => {
-    Axios.length("https://catfact.ninja/fact").then((res) => {
-      console.log(res);
-    });
+    dispatch({ type: "Fetch_start" });
+    Axios.get("https://catfact.ninja/fact")
+      .then((res) => {
+        dispatch({ type: fetch_success, date: res.date.fact });
+        console.log(res);
+      })
+      .catFact((error) => {
+        console.log(error);
+      });
   };
+  return (
+    <div>
+      <button onClick={handelFetch}>
+        {state.loading ? "is loading..." : "Fetch Cat Fact"}
+      </button>
+      {state.error && <p>Error, some thing is wrong</p>}
+      <h1>{fact}</h1>
+    </div>
+  );
 };
---
+++ src/factReducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  loading: false,
+  fact: "",
+  error: false,
+};
+export const factReducer = (state, action) => {
+  if (action.type === "Fetch_start")
+    return { loading: true, fact: "", error: false };
+  if (action.type === "Fetch_success")
+    return { loading: false, fact: action.date, error: false };
+  if (action.type === "Fetch_error")
+    return { loading: true, fact: "", error: true };
+};
